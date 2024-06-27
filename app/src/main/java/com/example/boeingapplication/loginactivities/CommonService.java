ffa39c5a9163e3c4c1890fc3b1e4092df5355dc0
koushik152 <koushiknaik340@gmail.com>
2024-06-27 16:35:38 +0530

    modified

--- app/src/main/java/com/example/boeingapplication/loginactivities/CommonService.java
+++ app/src/main/java/com/example/boeingapplication/loginactivities/CommonService.java
@@ -8,7 +8,7 @@ import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
-public interface CommonService {//hiibye
+public interface CommonService {
 
     @POST("rest/model/com/mobility/service/CommonService/login")
     Call<LoginResponse> login(@Body LoginRequest loginRequest);
